e2ef601477e2989eab8077298b22b3d85aca4ed0
miyasaka-t <cvcf10@cvcf.co.jp>
2025-07-08 11:30:00 +0900

    fix: ensure api/hello.ts is deployed

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,9 @@
+// pages/api/hello.ts
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   res.status(200).json({ message: 'こんにちは、APIからのレスポンスです！' })
 }
